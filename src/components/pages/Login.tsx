70e8ca631fe92aa01ad422cf88b9cb60b7dbcfc8
wsigma21 <wallstlbl@gmail.com>
2024-01-02 11:25:29 +0900

    feat: ログイン画面作成

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,17 @@
 import { FC, memo } from "react";
+import { Box, Button, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
 
 export const Login: FC = memo(() => {
-  return <p>ログインページです</p>;
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">ユーザー管理アプリ</Heading>
+        <Divider my={4} />
+        <Stack spacing={6} py={4} px={10}>
+          <Input placeholder="ユーザID" />
+          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>ログイン</Button>
+        </Stack>
+      </Box>
+    </Flex>  
+  );
 })
