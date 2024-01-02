d1d612a72a0b70814f4496fd7cdb497e15da3565
wsigma21 <wallstlbl@gmail.com>
2024-01-02 11:15:41 +0900

    feat: ヘッダのルーティング機能

--- src/components/molebules/MenuDrawer.tsx
+++ src/components/molebules/MenuDrawer.tsx
@@ -3,18 +3,21 @@ import { Drawer, DrawerOverlay, DrawerContent, DrawerBody, Button } from "@chakr
 
 type Props = {
   onClose: () => void,
-  isOpen: boolean
+  isOpen: boolean,
+  onClickHome: () => void,
+  onClickUserManagement: () => void,
+  onClickSetting: () => void,
 }
 
-export const MenuDrawer: FC<Props> = memo(({ onClose, isOpen}) => {
+export const MenuDrawer: FC<Props> = memo(({ onClose, isOpen, onClickHome, onClickUserManagement, onClickSetting }) => {
   return (
     <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
       <DrawerOverlay>
         <DrawerContent>
           <DrawerBody p={0} bg="gray.100">
-            <Button w="100%">TOP</Button>
-            <Button w="100%">ユーザ一覧</Button>
-            <Button w="100%">設定</Button>
+            <Button w="100%" onClick={onClickHome}>TOP</Button>
+            <Button w="100%" onClick={onClickUserManagement}>ユーザ一覧</Button>
+            <Button w="100%" onClick={onClickSetting}>設定</Button>
           </DrawerBody>
         </DrawerContent>
       </DrawerOverlay>
--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,10 +1,16 @@
-import { FC, memo } from "react";
+import { FC, memo, useCallback } from "react";
 import { Flex, Heading, Link, Box, useDisclosure } from "@chakra-ui/react";
 import { MenuIconButton } from "../../atoms/button/MenuIconButton";
 import { MenuDrawer } from "../../molebules/MenuDrawer";
+import { useNavigate } from "react-router-dom";
 
 export const Header: FC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const navigate = useNavigate();
+  const onClickHome = useCallback(() => navigate("/home"), [navigate])  ;
+  const onClickUserManagement = useCallback(() => navigate("/home/user_management"), [navigate])  ;
+  const onClickSetting = useCallback(() => navigate("/home/setting"), [navigate])  ;
+
   return (
     <>
       <Flex
@@ -15,17 +21,25 @@ export const Header: FC = memo(() => {
         justify="space-between"
         padding={{ base: 3, md: 5}}
       >
-        <Flex align="center" as="a" mr={8} _hover={{cursor: "pointer"}}>
-          <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>ユーザ管理アプリ</Heading>
+        <Flex align="center" as="a" mr={8} _hover={{cursor: "pointer"}} onClick={onClickHome}>
+          <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+            ユーザ管理アプリ
+          </Heading>
         </Flex>
         <Flex align="center" fontSize="sm" flexGrow={2} display={{ base: "none", md: "flex" }}>
           <Box pr={4}>
-            <Link>ユーザ一覧</Link>
+            <Link onClick={onClickUserManagement}>ユーザ一覧</Link>
           </Box>
-          <Link>設定</Link>
+          <Link onClick={onClickSetting}>設定</Link>
         </Flex>
         <MenuIconButton onOpen={onOpen} />
-        <MenuDrawer onClose={onClose} isOpen={isOpen}/>
+        <MenuDrawer 
+          onClickHome={onClickHome} 
+          onClickUserManagement={onClickUserManagement} 
+          onClickSetting={onClickSetting} 
+          onClose={onClose}
+          isOpen={isOpen}
+        />
       </Flex>
       
     </>
