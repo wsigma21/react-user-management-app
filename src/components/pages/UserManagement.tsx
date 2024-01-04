fec6c9632757773e0e513d80af1f48269a789dd1
wsigma21 <wallstlbl@gmail.com>
2024-01-04 21:25:33 +0900

    feat: ユーザ一覧

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,35 @@
-import { FC, memo } from "react";
+import { Wrap, WrapItem, Spinner, Center } from "@chakra-ui/react";
+import { FC, memo, useEffect } from "react";
+import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: FC = memo(() => {
-  return <p>ユーザ管理ページです</p>;
+  const { getUsers, users, loading} = useAllUsers();
+
+  // 初期マウント時1回だけ実行
+  useEffect(() => {
+    getUsers();
+  }, [getUsers]);
+
+  return (
+    <>
+      { loading ? (
+        <Center h="100vh">
+         <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard 
+                imageUrl="https://picsum.photos/800"
+                userName={user.username}
+                fullName={user.name}
+                />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
+  );
 })
